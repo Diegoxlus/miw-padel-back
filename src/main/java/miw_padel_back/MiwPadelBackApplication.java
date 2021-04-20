87ed5439a1126b746b8dd444bac9147e5bf74383
diegoxlus <lusky1996@gmail.com>
2021-04-21 00:33:14 +0200

    Initial commit

---
+++ src/main/java/miw_padel_back/MiwPadelBackApplication.java
@@ -0,0 +1,13 @@
+package miw_padel_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiwPadelBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiwPadelBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miw_padel_back/MiwPadelBackApplicationTests.java
@@ -0,0 +1,13 @@
+package miw_padel_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiwPadelBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
