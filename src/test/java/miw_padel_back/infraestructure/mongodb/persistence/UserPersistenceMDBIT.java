8bdabc4d0335f4e2a5b43e722f0291c67e4d6763
diegoxlus <lusky1996@gmail.com>
2021-04-23 21:30:17 +0200

    Add GithubActions compiling and test verify #MP-48

--- src/test/java/miw_padel_back/infraestructure/mongodb/persistence/UserPersistenceMDBIT.java
+++ src/test/java/miw_padel_back/infraestructure/mongodb/persistence/UserPersistenceMDBIT.java
@@ -2,7 +2,6 @@ package miw_padel_back.infraestructure.mongodb.persistence;
 
 import miw_padel_back.TestConfig;
 import miw_padel_back.domain.model.User;
-import miw_padel_back.infraestructure.mongodb.entities.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.test.StepVerifier;
