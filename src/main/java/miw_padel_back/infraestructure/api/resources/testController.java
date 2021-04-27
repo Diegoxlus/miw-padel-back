187a7ef759293028ee45085d2eab6c81f50c4d6a
diegoxlus <lusky1996@gmail.com>
2021-04-27 14:27:49 +0200

    Set yml for integration with AWS #MP-52

--- src/main/java/miw_padel_back/infraestructure/api/resources/testController.java
+++ src/main/java/miw_padel_back/infraestructure/api/resources/testController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/welcome")
     public String welcome(){
-        return "PRUEBA SPRING V1";
+        return "PRUEBA SPRING V2";
     }
 }
