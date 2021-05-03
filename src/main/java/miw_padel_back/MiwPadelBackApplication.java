cb3029e96b624548432bd9388d33600284bba59d
diegoxlus <lusky1996@gmail.com>
2021-05-04 00:18:07 +0200

    Set config and complete skeleton actions-config.yml #MP-55

--- src/main/java/miw_padel_back/MiwPadelBackApplication.java
+++ src/main/java/miw_padel_back/MiwPadelBackApplication.java
@@ -1,13 +1,10 @@
 package miw_padel_back;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class })
+@SpringBootApplication
 public class MiwPadelBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MiwPadelBackApplication.class, args);
 	}
