f54371a0a1cc50b0ca16a27523b1d6e43945d9ba
diegoxlus <lusky1996@gmail.com>
2021-05-03 15:42:34 +0200

    Set config and complete skeleton actions-config.yml #MP-55

---
+++ src/main/java/miw_padel_back/domain/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package miw_padel_back.domain.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    private static final String DESCRIPTION = "Not Found Exception";
+
+    public NotFoundException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+
+}
