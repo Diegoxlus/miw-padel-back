40350e9206f62803667d4bfdd52490a4d446bfaf
diegoxlus <lusky1996@gmail.com>
2021-06-07 12:52:01 +0200

    Refactor image profile

--- src/main/java/miw_padel_back/infraestructure/api/resources/UserResource.java
+++ src/main/java/miw_padel_back/infraestructure/api/resources/UserResource.java
@@ -49,7 +49,7 @@ public class UserResource {
 
     @SecurityRequirement(name = "bearerAuth")
     @PreAuthorize("hasRole('PLAYER') or hasRole('ADMIN')")
-    @PostMapping(value = PHOTO, consumes = {"multipart/form-data"}, produces = MediaType.IMAGE_PNG_VALUE)
+    @PostMapping(value = PHOTO, consumes = {"multipart/form-data"})
     public Mono<Void> create(@RequestPart(value = "file") FilePart file) {
 
         return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
